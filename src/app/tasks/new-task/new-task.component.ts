a50241d3ccc66cd77581d9ae078ac1251ee81d8e
Daniel Goins <dwgoins98@gmail.com>
2025-02-13 23:14:23 -0500

    Added comments to task components
    - src/app/tasks/new-task/new-task.component.ts
    - src/app/tasks/tasks-list/tasks-list.component.css
    - src/app/tasks/tasks-list/tasks-list.component.html
    - src/app/tasks/tasks.component.ts

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,3 +1,12 @@
+/**
+ * @fileoverview Component responsible for creating new tasks in the task manager application.
+ *
+ * @description This component provides a form to input new task details and adds the task to the task list using the TasksService.
+ *
+ * @example
+ * <app-new-task></app-new-task>
+ */
+
 import { Component, ElementRef, inject, viewChild } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
@@ -13,6 +22,13 @@ export class NewTaskComponent {
   private tasksService = inject(TasksService);
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
 
+  /**
+   * Adds a new task using the provided title and description.
+   * Resets the form after the task is added.
+   *
+   * @param title - The title of the new task.
+   * @param description - The description of the new task.
+   */
   onAddTask(title: string, description: string) {
     this.tasksService.addTask({ title: title, description: description });
     this.formEl()?.nativeElement.reset();
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -4,7 +4,7 @@ import { TasksListComponent } from './tasks-list/tasks-list.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [NewTaskComponent, TasksListComponent],
+  imports: [NewTaskComponent, TasksListComponent],      // Imports 2 components for the template to use
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
